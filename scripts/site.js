0b3dee10e770f8d8daeac49964fe775cc912f409
MrPIvanov <MrPIvanov1117@gmail.com>
2019-08-08 13:41:33 +0300

    Fix some bugs before starting with landing page

---
+++ scripts/site.js
@@ -0,0 +1,9 @@
+// let elements = Array.from(document.querySelectorAll("div.single-news"));
+
+// for (const element of elements) {
+//     element.addEventListener("mousedown", function (e) {
+
+//         this.classList.add("readIt");
+
+//     });
+// }
